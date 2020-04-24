64de31ea56cb5b18ea14d663eff49f53cdbb8acd
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-04-24 11:28:43 -0500

    Adding logic to execute shell script

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const cp = require('child_process')
 exports.prepareReport = function() {
-    cp.spawn('formatReport', ['./formatReport.sh'])
+    cp.exec("chmod", "chmod 744 formatReport.sh", function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        } 
+    })
+
+    cp.exec("formatReport", "./formatReport", function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        } 
+    })
 }
